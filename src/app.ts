57e0dac83129d747a558d4624e8c4ae403c9be00
Rai-Carvalho <raiarturc@gmail.com>
2024-09-20 21:32:26 -0300

    add extended: true

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import { getOpenAICompletion } from "./services/openai"
 
 const app = express()
 
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 app.use(cors())
